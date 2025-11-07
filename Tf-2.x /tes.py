ff97d1a1acce7ab94b4f533e983af2f8564256c3
celery <thienquan3206@gmail.com>
2025-11-08 00:14:25 +0800

    fold 3

--- Tf-2.x /tes.py
+++ Tf-2.x /tes.py
@@ -510,7 +510,7 @@ def main():
     # --------------------------
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
     all_results = [] #store results from all folds
-    target_fold = 2
+    target_fold = 3
     # Get train/test indices for target fold (config['fold'] = 4)
     for fold_idx, (train_index, test_index) in enumerate(skf.split(subject_IDs, label_list)):
 
